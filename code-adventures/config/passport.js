8589ce59d77d37bd8c9209c0433d39202c138c7d
matthew woodward <matthew.woodward1982@gmail.com>
2019-03-25 19:23:22 -0700

    arranging file structure

---
+++ code-adventures/config/passport.js
@@ -0,0 +1,107 @@
+//load bcrypt
+let bCrypt = require("bcrypt-nodejs"); //secures our password
+
+module.exports = function (passport, User) {
+    var User = User; //initializes user model which gets passed as an argument
+
+    let LocalStrategy = require("passport-local").Strategy; //initializes the passport-local strategy
+
+    //serialize to save user id in the session to retrieve user details when needed
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+    // deserialize user
+    passport.deserializeUser(function (id, done) {
+        User.findByPk(id).then(function (user) {
+            if (user) {
+                done(null, user.get());
+            } else {
+                done(user.errors, null);
+            }
+        });
+    });
+    passport.use(
+        "local-signup",
+        new LocalStrategy(
+            {
+                usernameField: "userName", //requesting the user-name field to create passport variable.
+
+                passwordField: "password", //requesting the password field to create passport variable.
+
+                passReqToCallback: true // allows to pass back the entire request to the callback
+            },
+
+            function (req, userName, password, done) {
+                let generateHash = function (password) {
+                    return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+                };
+
+                User.findOne({
+                    where: {
+                        userName: userName
+                    }
+                }).then(function (user) {
+                    if (user) {
+                        return done(null, false, {
+                            message: "That username is already taken"
+                        });
+                    } else {
+                        let userPassword = generateHash(password); //protects password
+                        let data = {
+                            userName: userName,
+                            password: userPassword
+                        };
+
+                        User.create(data).then(function (newUser, created) {
+                            if (!newUser) {
+                                return done(null, false);
+                            }
+
+                            if (newUser) {
+                                return done(null, newUser);
+                            }
+                        });
+                    }
+                });
+            }
+        )
+    );
+
+    passport.use(
+        "local-signin",
+        new LocalStrategy(
+            {
+                usernameField: "userName", //requesting the user-name field to create passport variable.
+
+                passwordField: "password", //requesting the password field to create passport variable.
+
+                passReqToCallback: true // allows to pass back the entire request to the callback
+            },
+
+            function (req, userName, password, done) {
+                const isValidPassword = function (userpass, password) {
+                    return bCrypt.compareSync(password, userpass);
+                };
+
+                User.findOne({
+                    where: {
+                        userName: userName
+                    }
+                }).then(function (user) {
+                    if (user) {
+                        if (isValidPassword(user.password, password)) {
+                            return done(null, user);
+                        }
+                        return done(null, false, {
+                            message: "The username and passwod are not correct"
+                        });
+                    } else {
+                        return done(null, false, {
+                            message: "The username and passwod are not correct"
+                        });
+                    }
+                });
+            }
+        )
+    );
+};
---
+++ code-adventures/routes/apiRoutes.js
@@ -0,0 +1,50 @@
+let db = require("../models");
+
+let passport = require("passport");
+
+let unirest = require("unirest");
+
+const parseString = require("xml2js").parseString;
+
+module.exports = function (app) {
+    // User login routes.
+    app.post(
+        "/signin",
+        passport.authenticate("local-signin", {
+            successRedirect: "/translate",
+            failureRedirect: "/"
+        })
+    );
+    app.post(
+        "/signup",
+        passport.authenticate("local-signup", {
+            successRedirect: "/"
+        }),
+        (req, res, next) => {
+            if (!req.user) {
+                res.json({ success: false });
+            }
+        }
+    );
+    // Get all Users
+    app.get("/api/Users", function (req, res) {
+        db.User.findAll({}).then(function (dbUsers) {
+            res.json(dbUsers);
+        });
+    });
+
+    // Create a new user
+    app.post("/api/Users", function (req, res) {
+        db.User.create(req.body).then(function (data) {
+            res.json(data);
+        });
+    });
+
+    // Delete a user by id
+    app.delete("/api/Users/:id", function (req, res) {
+        db.User.destroy({ where: { id: req.params.id } }).then(function (data) {
+            res.json(data);
+        });
+    });
+
+};
